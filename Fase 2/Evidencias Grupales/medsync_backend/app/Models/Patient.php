836800aacf13f4b2576cd0831388ee4ec7c5eb5b
Roberto Acuña <ro.acuna@duocuc.cl>
2026-09-04 12:56:13 -0400

    feat: modelos eloquent para autenticacion de pacientes

---
+++ Fase 2/Evidencias Grupales/medsync_backend/app/Models/Patient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Patient extends Model
+{
+    protected $table = 'patient';
+    public $timestamps = false;
+    protected $fillable = [
+        'users_id', 'health_insurance_id', 'first_name',
+        'last_name', 'rut', 'birth_date', 'phone',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function healthInsurance()
+    {
+        return $this->belongsTo(HealthInsurance::class, 'health_insurance_id');
+    }
+}
