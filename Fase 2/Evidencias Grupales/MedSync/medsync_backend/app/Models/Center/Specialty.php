44c015f6bfa11d9edd1827b9594c546fa06584f4
Roberto Acuña <ro.acuna@duocuc.cl>
2026-09-25 13:57:58 -0300

    feat(database): agregar servicios por especialidad

--- Fase 2/Evidencias Grupales/MedSync/medsync_backend/app/Models/Center/Specialty.php
+++ Fase 2/Evidencias Grupales/MedSync/medsync_backend/app/Models/Center/Specialty.php
@@ -4,6 +4,7 @@ namespace App\Models\Center;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Specialty extends Model
 {
@@ -51,4 +52,14 @@ class Specialty extends Model
             ->using(ProfessionalSpecialty::class)
             ->withTimestamps();
     }
+    /**
+ * Prestaciones asociadas a la especialidad.
+ */
+public function services(): HasMany
+{
+    return $this->hasMany(
+        Service::class,
+        'specialty_id'
+    );
+}
 }
